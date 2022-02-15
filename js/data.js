d746c9d7b10120eb3306f3e7ba8452d7d44c4428
Daniel Catan <danieljcatan@gmail.com>
2022-02-14 17:34:06 -0800

    local storage saving entries

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataJSON = localStorage.getItem('data-local-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function beforeUnloadListener(event) {
+  var $data = JSON.stringify(data);
+  localStorage.setItem('data-local-storage', $data);
+}
+
+window.addEventListener('beforeunload', beforeUnloadListener);
